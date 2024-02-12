e9c4f894fdae871f678a12556c4b34760b86219f
saip <sai-s@siemens.com>
2024-02-12 14:17:10 +0530

    feature update

--- main/views.py
+++ main/views.py
@@ -5,13 +5,13 @@ from .forms import FileModelForm
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor, plot_tree
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, accuracy_score
 from .data_prep_pipeline import DataPrep 
 from .data_preprocessing_pipeline import DataPreprocessor
 from .data_splitting import DataSplitter
 from .purge import PurgeDirectory
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
+# from keras.models import Sequential
+# from keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt  
 import numpy as np    
 import seaborn as sns
@@ -191,6 +191,7 @@ def train_model(request):
             
             #eval_metrics['Mean Square Error']=mse
             eval_metrics['Mean Absolute Error']=mae
+
             #eval_metrics['Root Mean Square Error']=rmse
             eval_metrics['R2 Score']=r2
             saved_file=os.path.join(settings.BASE_DIR,'data\\modelsDump\\') +filename+'.joblib'
@@ -246,6 +247,7 @@ def train_model(request):
             model.fit(X_train,y_train)
             y_pred=model.predict(X_test)
             mae=round(mean_absolute_error(y_true=y_test,y_pred=y_pred),2)
+           
             eval_metrics['Mean Absolute Error']=mae
             plot_name=None
             saved_file=os.path.join(settings.BASE_DIR,'data\\modelsDump\\') +filename+'.joblib'
